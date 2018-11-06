468b3687c7a3ad0fd675e4c9f9df299177a1181f
vipulchhabra99 <vipulchhabra555@gmail.com>
2018-11-06 13:54:29 +0530

    index-page

---
+++ index-login.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <title>LOGIN PAGE </title>
+        <link href='css/dtyle.css' rel='stylesheet' type='text/css'>
+    </head>
+
+    <script>
+        
+        function field_focus(field, email)
+    {
+        if(field.value == email)
+        {
+            field.value = '';
+        }
+    }
+
+      function field_blur(field, email)
+      {
+        if(field.value == '')
+        {
+          field.value = email;
+        }
+      }
+        
+        
+        
+        
+        </script>
+
+<body>
+<img src=""
+<form method="post" action="login-pass.php">
+
+<div class="box">
+    <img src="images/Webp.net-resizeimage.png">
+<h1>LOGIN</h1>
+
+<input type="email" name="login-email" value="email" onFocus="field_focus(this, 'email');" onblur="field_blur(this, 'email');" class="email" />
+  
+<input type="password" name="login-password" value="email" onFocus="field_focus(this, 'email');" onblur="field_blur(this, 'email');" class="email" />
+    
+    <input type="hidden" value="yes" name="validate">
+    
+    <input type="submit" name="login-submit" class="submitabc">
+  
+<!--<a href="#"><div class="btn">Sign In</div></a> <!-- End Btn -->
+
+<!--<a href="#"><div id="btn2">Sign Up</div></a> <!-- End Btn2 -->
+  
+</div> <!-- End Box -->
+  
+</form>
+
+</body>
+  
+
+</html>
