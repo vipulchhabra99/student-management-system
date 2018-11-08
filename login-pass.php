a8963ea264b6cdaa22b4d1163d99d277ff897870
vipulchhabra99 <vipulchhabra555@gmail.com>
2018-11-08 22:14:35 +0530

    error-pages

--- login-pass.php
+++ login-pass.php
@@ -8,7 +8,7 @@ if(isset($_POST['validate']))
     $sql="SELECT * FROM login";
 
     $run_sql=mysqli_query($conn,$sql);
-
+    $flag=0;
     $usid=$_POST['login-email'];
     $uspas=$_POST['login-password'];
 
@@ -16,22 +16,22 @@ if(isset($_POST['validate']))
         if($rows['user-email']==$usid){
             if($rows['user-password']==$uspas)
             {
-                
+                $flag=1;
                 $_SESSION["username"]=$rows['user-name'];
                 $_SESSION["photo"]=$rows['user-picture'];
                 header("Location:amin-panel.php");
+                break;
                 
             }
             
-            else{
-                
-            }
+
         }
         
-        else{
-            
-        }
-        
+    }
+    
+    if($flag==0)
+    {
+        header("Location:login-error.html");
     }
 
 
